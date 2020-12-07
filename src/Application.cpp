ab2339cfe2c45ff43115414d6bd2cf5ffc9da8dc
Jiwon Kang <jwkorange@gmail.com>
2020-12-07 15:10:18 +0900

    [Add] loadModel() function

--- src/Application.cpp
+++ src/Application.cpp
@@ -819,6 +819,43 @@ void Application::createTextureSampler()
 
 void Application::loadModel()
 {
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string war, err;
+
+	if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &war, &err, MODEL_PATH.c_str()))
+	{
+		throw std::runtime_error(war + err);
+	}
+
+	for (const auto& shape : shapes)
+	{
+		for (const auto& index : shape.mesh.indices)
+		{
+			Vertex vertex{};
+
+			vertex.pos =
+			{
+				attrib.vertices[3 * index.vertex_index + 0],
+				attrib.vertices[3 * index.vertex_index + 1],
+				attrib.vertices[3 * index.vertex_index + 2]
+			};
+
+			vertex.color = { 0.0f, 0.0f, 0.0f };
+
+			vertex.texCoord =
+			{
+				attrib.texcoords[2 * index.texcoord_index + 0],
+				1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+			};
+
+			vertices.push_back(vertex);
+			indices.push_back(indices.size());
+		}
+	}
+
+
 }
 
 void Application::createVertexBuffers()
